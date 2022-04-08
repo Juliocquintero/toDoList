bdc732a8a1b67ffc74b52dace01a1b773375195a
Julioq <juliocquinterog17@gmail.com>
2022-04-08 11:05:43 -0400

    prueba

--- src/ui/App/App.js
+++ src/ui/App/App.js
@@ -1,10 +1,18 @@
-import { ToDoProvider } from '../../hoc/toDoContext';
-import '../../styles/App.css';
-import Date from '../components/Date';
-import FormComponent from '../components/Form';
-import ToDoList from '../components/TaskList';
+import { ToDoProvider } from "../../hoc/toDoContext";
+import "../../styles/App.css";
+import Date from "../components/Date";
+import FormComponent from "../components/Form";
+import ToDoList from "../components/TaskList";
 
 const App = () => {
+  screen.orientation
+    .lock("landscape")
+    .then(() => {
+      alert("Hola");
+    })
+    .catch((error) => {
+      console.log("fail");
+    });
   return (
     <div className="App">
       <h1> To Do List </h1>
