05e1aff1e2bf83a205097741c73a7e13231956fd
Julioq <juliocquinterog17@gmail.com>
2022-04-13 11:06:18 -0400

    Pruebas

--- src/ui/App/App.js
+++ src/ui/App/App.js
@@ -3,19 +3,50 @@ import "../../styles/App.css";
 import Date from "../components/Date";
 import FormComponent from "../components/Form";
 import ToDoList from "../components/TaskList";
+import React from "react";
 const { screen } = window;
 const App = () => {
-  screen.orientation
-    .lock("landscape")
-    .then(() => {
-      alert("Success");
-    })
-    .catch((error) => {
-      alert("Error");
-    });
+  // React.useEffect(() => {
+  //   screen.orientation
+  //     .lock("landscape")
+  //     .then(() => {
+  //       alert("Success");
+  //     })
+  //     .catch((error) => {
+  //       alert("Error");
+  //     });
+  //   return () => {};
+  // }, []);
+
+  const getOrientation = () => window.screen.orientation.type;
+  const lockOrientation = () => {
+    screen.orientation
+      .lock("landscape")
+      .then(() => {
+        console.log("Success");
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  const [orientation, setOrientation] = React.useState(getOrientation());
+
+  const updateOrientation = (event) => {
+    lockOrientation();
+    setOrientation(getOrientation());
+  };
+
+  React.useEffect(() => {
+    window.addEventListener("orientationchange", updateOrientation);
+    return () => {
+      window.removeEventListener("orientationchange", updateOrientation);
+    };
+  }, []);
+
   return (
     <div className="App">
-      <h1> To Do List </h1>
+      <h1> To Do List {orientation} </h1>
       <Date />
       <ToDoProvider>
         <FormComponent />
